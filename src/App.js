7e08e4c81057f31368bd107109be2f48be526fce
Burcu Bayraö <burju7@yahoo.com>
2023-06-15 17:44:57 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
   useEffect(() => {
+    document.title = text;
+
     console.log("useEffect called");
-  });
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        {" "}
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <DogPics />
+    </>
+  );
 }
 
 export default App;
--- src/DogPics.js
+++ src/DogPics.js
@@ -13,14 +13,14 @@ function DogPics() {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  }, []);
 
   console.log("render");
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt="" />
       ))}
     </div>
   );
